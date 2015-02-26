968cbd7b36f6d9715083105e19843771286ffd87
Nan Wang <nantas@gmail.com>
2015-02-26 18:42:35 +0800

    closed fireball-x/fireball-site#12 added offline apidocs into the repo

---
+++ apidocs/api.js
@@ -0,0 +1,32 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CallbacksHandler",
+        "CallbacksInvoker",
+        "Color",
+        "Component",
+        "FObject",
+        "Fire",
+        "Fire.Scene",
+        "Fire._DeserializeInfo",
+        "HashObject",
+        "Intersection",
+        "Math",
+        "Matrix23",
+        "Path",
+        "Rect",
+        "Transform",
+        "Vec2"
+    ],
+    "modules": [
+        "Fire"
+    ],
+    "allModules": [
+        {
+            "displayName": "Fire",
+            "name": "Fire",
+            "description": "Fire module hoding global functions"
+        }
+    ]
+} };
+});
